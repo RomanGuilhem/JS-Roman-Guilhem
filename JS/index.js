45134eb67e615fa5941682b1e7a18e8e65a144c3
Roman Guilhem <romanguilhem@hotmail.com>
2024-06-11 21:38:58 -0300

    segunda version proyecto final

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,51 @@
-const MARCA = prompt ("ingrese la marca de su vehiculo");
-const MODELO = parseInt(prompt ("ingrese el año de su vehiculo"));
-const SEGUROBASE = 50000;
-const COTIZARAÑO = 2000;
-const COTIZARSEGURO = (SEGUROBASE , PRECIOAÑO) => SEGUROBASE * PRECIOAÑO; 
-const PRECIOAÑO = (MODELO, COTIZARAÑO) => MODELO * COTIZARAÑO;
+const Marca = prompt ("ingrese la marca de su vehiculo");
+const Modelo = parseInt (prompt ("ingrese el año de su vehiculo"));
+const KM = parseInt (prompt ("ingrese el kilometraje de su vehiculo"));
+const SeguroBase = 50000;
+const CotizarAño = 2;
+const Vehiculo = {
+    "PEUGEOT" : 1.5,
+    "RENAULT" : 1.2,
+    "FIAT" : 1.2,
+    "FORD" : 1.5,
+    "VOLSKWAGEN" : 1.2,
+    "BMW" : 2,
+    "AUDI" : 2
+};
 
-do{
-if(isNaN(MODELO)){
+const Nuevo = 7;
+const Usado = 5;
+const Antiguo = 3;
+
+const Kilometraje={
+    Nuevo : KM >= 5000,
+    Usado : 5000 < KM >= 100000,
+    Antiguo : KM > 100000
+};
+const PrecioAño = (Modelo, CotizarAño) => (Modelo * CotizarAño);
+const PrecioModelo = (Vehiculo,SeguroBase) => (Vehiculo * SeguroBase)
+const Cotizar = (SeguroBase , PrecioAño, PrecioModelo) => (SeguroBase * PrecioAño * PrecioModelo); 
+const CotizarPorKm = (Cotizar, Kilometraje) => (Cotizar * Kilometraje)
+
+
+function CotizarVehiculo() {
+    if(isNaN(Modelo)){
     alert(`Ingrese un numero por favor`);
-    break;
-} 
-else if(2015>MODELO){
+    } 
+    else if(2015>Modelo){
     console.log("su vehiculo unicamente puede contratar seguro contra terceros por un valor de $40000");
-}
-else{
+    }
+    else{
     console.log("su vehiculo es adecuado para contratar cualquier plan de seguro");
+    return CotizarSeguro()
+    }
 }
-} while (MODELO<2025){
-    COTIZARSEGURO;
+
+function CotizarSeguro() {
+    PrecioAño();
+    PrecioModelo();
+    Cotizar();
+    CotizarPorKm();
 }
+
+CotizarVehiculo();
